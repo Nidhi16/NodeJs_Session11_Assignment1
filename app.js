0ab65dca69f3ba1792b2c61730891b6a6f86396c
Nidhi16 <nidhi.valuable@gmail.com>
2017-12-07 23:17:02 +0530

    Added comments and screenshots

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
-var app = express();
+var app = express();  
 var jade = require('jade');
 
 var port = process.env.PORT || 8000;
 
 app.get('/', function(request, response) {
+    // Calling jade API to render file
     var content = jade.renderFile('./views/index.jade', {'name': 'Nidhi Sinha'});
-    response.send(content);
+    // used express api send to send data
+    response.status(200).send(content);
 });
 
+// Listen to desired port number
 app.listen(port, function() {
     console.log("Listening on port " + port);
 });
